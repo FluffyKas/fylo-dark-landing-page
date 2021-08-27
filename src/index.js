5f1fbdf2a025f2dd0592d34da7bb89bcb5f2e8e9
FluffyKas <maruzs.zso@gmail.com>
2021-08-27 14:11:00 +0200

    first file upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".cta__form");
+const email = document.querySelector(".email");
+const errorMessage = document.querySelector(".error-message");
+ 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  checkInput();
+});
+
+function checkInput() {
+
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    errorMessage.className = "error-message error";
+    errorMessage.innerText ="Email cannot be empty";
+  } else if (!isItEmail(emailValue)) {
+    errorMessage.className = "error-message error";
+    errorMessage.innerText = "Looks like this is not an email";
+  } else {
+    alert("Yay! Success!");
+  };
+};
+
+//checking email input validity
+function isItEmail (email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+};
